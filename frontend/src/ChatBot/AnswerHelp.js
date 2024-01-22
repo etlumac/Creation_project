e23255d51feea6e60d809b84d435c9c43a75406e
Vladislav <chechetkovlad@mail.ru>
2024-01-22 09:42:03 +0300

    modified:   frontend/package.json 	new file:   frontend/src/ChatBot/AnswerHelp.js 	modified:   frontend/src/ChatBot/config.js

---
+++ frontend/src/ChatBot/AnswerHelp.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function AnswerHelp(props){
+    return(
+        <div>
+            <button className="start-answer">Yes</button>
+            <button className="start-answer">No</button>
+        </div>
+    )
+}
--- frontend/src/ChatBot/config.js
+++ frontend/src/ChatBot/config.js
@@ -1,14 +1,20 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 import AnswerYes from './AnswerYes';
+import AnswerHelp from './AnswerHelp';
 const config = {
   botName: "Creation",
-  initialMessages: [createChatBotMessage(`Здравствуйте, могу ли я вам помочь?`)],
+  initialMessages: [createChatBotMessage(`Здравствуйте, есть ли у вас вопросы?`,{widget:'AnswerHelp'})],
   widgets: [
     {
       widgetName: 'Answer',
       widgetFunc: (props) => <AnswerYes {...props} />,
       mapStateToProps: ['gist'],
     },
+    {
+      widgetName: 'QustionHelp',
+      widgetFunc: (props) => <AnswerHelp {...props} />,
+      mapStateToProps: ['gist'],
+    }
     
   ]
   
