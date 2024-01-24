e65b0c8876859196b3ca0f581bfc2d33aae6c77b
Andrey Petrenko <36794228+Deadshotop@users.noreply.github.com>
2024-01-24 20:51:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,11 @@ const app = express()
 const PORT = process.env.PORT || 3001
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/
 
+const TelegramBot = require('node-telegram-bot-api');
+const botToken = '6789866050:AAEe0c7DYXEX0BxckucOg0Q9cD-ojhYmiyg';
+const bot = new TelegramBot(botToken, { polling: true });
+const chatId = '-4190366568'; 
+
 app.use(bodyParser.json())
 
 const dataFilePath = path.join(__dirname, 'admin.txt')
@@ -35,8 +40,9 @@ app.post('/api/submitForm', (req, res) => {
     message,
     timestamp: formatTimestamp(new Date())
   }
-
+  
   const formDataString = JSON.stringify(formData, null, 2)
+  bot.sendMessage(chatId, 'Новый результат формы: ' + formDataString);
   fs.appendFileSync(dataFilePath, `${formDataString}\n`, 'utf8')
 
   res.status(200).json({ success: true, message: 'Form data submitted successfully' })
