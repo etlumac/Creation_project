7a9bc5f05308e9a9171e6f3f18233835438efd71
Anatoly-Ko <korop4enko.tolik@yandex.ru>
2024-01-27 15:20:19 +0300

    tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,46 @@
-// @ts-check
 const { test, expect } = require('@playwright/test')
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+  await page.goto('http://localhost:3000/home')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+  await expect(page).toHaveTitle(/React App/)
 })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
+test('get russia link', async ({ page }) => {
+  await page.goto('http://localhost:3000/home')
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click()
+  await page.getByRole('banner').getByRole('link', { name: 'Russia' }).click()
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible()
+  await expect(page.getByRole('heading', { name: 'Russia' })).toBeVisible()
+})
+
+test('get algeria link', async ({ page }) => {
+  await page.goto('http://localhost:3000/home')
+
+  await page.getByRole('banner').getByRole('link', { name: 'Algeria' }).click()
+
+  await expect(page.getByRole('heading', { name: 'Algeria' })).toBeVisible()
+})
+
+test('get faq link', async ({ page }) => {
+  await page.goto('http://localhost:3000/home')
+
+  await page.getByRole('banner').getByRole('link', { name: 'FAQ' }).click()
+
+  await expect(page.getByRole('heading', { name: 'Frequantly Asked Questions' })).toBeVisible()
+})
+
+test('testing form submitting and clearing inputs after submit', async ({ page }) => {
+  await page.goto('http://localhost:3000/home')
+  await page.fill('input[name="name"]', 'Mr test')
+  await page.fill('input[name="email"]', 'test@test.com')
+  await page.fill('textarea[name="message"]', 'Test')
+  await page.click('button[type="submit"]')
+  await page.waitForTimeout(3000)
+
+  await expect(page.getByRole('textbox', { name: 'name' })).toBeEmpty
+  await expect(page.getByRole('textbox', { name: 'email' })).toBeEmpty
+  await expect(page.getByRole('textbox', { name: 'message' })).toBeEmpty
+
+  console.log('success Form test')
 })
