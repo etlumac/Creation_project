1e8f623f5ab731230d09e189b5d3b6c7c964996c
EternalVelard <chechetkovlad@mail.ru>
2024-02-03 22:46:50 +0300

    Revert "Merge branch 'main' of https://github.com/etlumac/Creation_project"
    This reverts commit 1e61693b8001d62ef422ee04fd91c64ebb65c3f3, reversing
    changes made to c4b78c63a096c713af5341656b4de46547626906.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-// import { createRoot } from 'react-dom'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom'
 import { App } from './App'
 
 const root = createRoot(document.getElementById('root'))
@@ -9,5 +8,5 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  // document.getElementById('root')
+  document.getElementById('root')
 )
