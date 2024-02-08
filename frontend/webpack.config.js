28d7254a0b584a3d37e1297d93e1e00354c17589
EternalVelard <chechetkovlad@mail.ru>
2024-02-08 13:15:35 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/etlumac/Creation_project""
    This reverts commit 1e8f623f5ab731230d09e189b5d3b6c7c964996c.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,60 +1,60 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    devServer: {
-        compress: false,
-        historyApiFallback: {
-            disableDotRule: true,
-          },
-      },
-      watchOptions: {
-        poll: 1000,
-      },
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(mp4)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            cacheDirectory: false,
-          },
-        },
-      },
+// module.exports = {
+//     devServer: {
+//         compress: false,
+//         historyApiFallback: {
+//             disableDotRule: true,
+//           },
+//       },
+//       watchOptions: {
+//         poll: 1000,
+//       },
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     publicPath: '/dist/',
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(png|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//       },
+//       {
+//         test: /\.(mp4)$/i,
+//         type: 'asset/resource',
+//       },
+//       {
+//         test: /\.jsx?$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-react', '@babel/preset-env'],
+//             cacheDirectory: false,
+//           },
+//         },
+//       },
      
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-  ],
-};
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: path.resolve(__dirname, 'public/index.html'),
+//     }),
+//   ],
+// };
